79b4d6ca8c6d28fc9795798b697eb537550eb7f0
ElenaSloth10 <ymnutsa10@gmail.com>
2022-07-09 15:09:33 +0300

    temperatureIndicator

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function showTemt(response) {
+  console.log(response);
+  let temperatureIndicator = document.querySelector("#temperature");
+  temperatureIndicator.innerHTML = Math.round(response.data.main.temp);
+  let cityIndicator = document.querySelector("h1");
+  cityIndicator.innerHTML = response.data.name;
+  let weatherSkyIndicator = document.querySelector("#weatherSky");
+  weatherSkyIndicator.innerHTML = response.data.weather[0].description;
+  let humidityIndicator = document.querySelector("#humidity");
+  humidityIndicator.innerHTML = response.data.main.humidity;
+  let windIndicator = document.querySelector("#wind");
+  windIndicator.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let city = "Paris";
 let apiKey = "905addfdd47350a84b1fff2eb7da7fbb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`;
 
