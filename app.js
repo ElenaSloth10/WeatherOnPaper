1f5638e619dee106c689340eb7ede59ece97b4aa
ElenaSloth10 <ymnutsa10@gmail.com>
2022-07-09 16:24:02 +0300

    dateIndicator

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemt(response) {
   console.log(response);
   let temperatureIndicator = document.querySelector("#temperature");
@@ -10,6 +34,9 @@ function showTemt(response) {
   humidityIndicator.innerHTML = response.data.main.humidity;
   let windIndicator = document.querySelector("#wind");
   windIndicator.innerHTML = Math.round(response.data.wind.speed);
+  let dateIndicator = document.querySelector("h3");
+  dateIndicator.innerHTML = formatDate(response.data.dt * 1000);
+
   let skyIconIndicator = document.querySelector("#skyIcon");
   skyIconIndicator.setAttribute(
     "src",
@@ -18,7 +45,7 @@ function showTemt(response) {
   skyIconIndicator.setAttribute("alt", response.data.weather[0].description);
 }
 
-let city = "Kyiv";
+let city = "Washington";
 let apiKey = "905addfdd47350a84b1fff2eb7da7fbb";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=`;
 
